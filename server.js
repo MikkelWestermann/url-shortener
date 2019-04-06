e0114e0952c0f593f8cb67628a430bbbc3b7c032
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2019-04-06 12:04:42 +0200

    Update docker compose

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Hello World')
+    console.log('Hello World!')
     res.send('<h1>Helloooo World!</h1>')
 })
 
