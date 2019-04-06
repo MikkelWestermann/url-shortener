f1b44b71c56466e40c4b94af708fa57941aba4c1
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2019-04-06 11:40:05 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'); 
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('Hello World')
+    res.send('<h1>Helloooo World!</h1>')
+})
+
+const PORT = process.env.PORT || 1235;
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+})
