c9953b9f7605f6a94f14dc7dc229fa9a9875867f
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-04-06 23:49:30 +0200

    Add comments

--- server.js
+++ server.js
@@ -16,36 +16,51 @@ app.get("/", (req, res) => {
 
 app.get("/:code", (req, res) => {
   const { code } = req.params;
-  new Promise((resolve, reject) =>
+	new Promise((resolve, reject) =>
+		// Retrieve value from redis db where key is code
     redisClient.get(code, (err, reply) => {
+			// Check response from redis
       !err ? resolve(reply) : reject(err);
     })
   )
     .then(result => {
+			// Check that response is not null and redirect 
       result ? res.redirect(result) : res.send("Link has expired")
     })
     .catch(err => res.send("Not a valid link"));
 });
 
 app.post("/create-tiny", async (req, res) => {
-  const { url, duration } = req.body;
+	const { url, duration } = req.body;
+	// Check that the url is valid
   if (validUrl.isUri(url)) {
     let safety = 0;
-    let hasFoundCode = false;
+		let hasFoundCode = false;
+		// Loop insertion of key/value in db to make sure
+		// there are no dublicate keys in the db. 
+		// side note: 100 might be a bit excessive, but better
+		// safe than sorry, right...
     while (safety < 100) {
+			// Generate a random string of characters
       const urlCode = shortid.generate();
-      await new Promise((resolve, reject) =>
+			await new Promise((resolve, reject) =>
+				// Check if the url code already exsists in the db
         redisClient.exists(urlCode, (err, reply) => {
           !err ? resolve(reply) : reject(err);
         })
       )
         .then(async reply => {
+					// Check that the reply is 0 <- meaning that the 
+					// url code does not exist in the db
           if (!reply) {
-            await new Promise((resolve, reject) =>
+						await new Promise((resolve, reject) =>
+							// Insert the url code and url as key/value in db
+							// with an expiration of duration <- in seconds
               redisClient.set(urlCode, url, "EX", duration, (err, reply) => {
                 !err ? resolve(reply) : reject(err);
               })
             ).then(() => {
+							// Everything was successful!!!
               hasFoundCode = true;
               res.send(urlCode);
             });
@@ -56,7 +71,8 @@ app.post("/create-tiny", async (req, res) => {
         break;
       }
       safety++;
-    }
+		}
+		// It was not possible to find a url code that did not exist in the db already
     if (safety === 100) {
       res.status(409).send("Could not store url code. Please try again");
     }
